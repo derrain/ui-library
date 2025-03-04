1764c3b748a01754a988b2a3e2f7e059222d0099
Derrain de Silva <derrain007@gmail.com>
2025-03-05 09:49:15 +1300

    Implemented the Navbar component

--- playground/components/Navbar.vue
+++ playground/components/Navbar.vue
@@ -1,1 +1,48 @@
-<template></template>
+<template>
+  <header>
+    <nav>
+      <router-link to="/" class="navbar-brand">
+        <h1>
+          <span class="special-letter">N</span>uxt <span class="special-letter">U</span>ser <span class="special-letter">E</span>xperience <span class="special-letter">D</span>esign
+        </h1>
+      </router-link>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  @use '~/assets/colors' as *;
+
+  header {
+    background: $black;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 1rem;
+    position: sticky;
+    top: 0;
+    padding: 1rem 1rem .5rem;
+    box-sizing: border-box;
+
+    nav {
+      width: fit-content;
+
+      .navbar-brand {
+        color: $white;
+        text-decoration: none;
+
+        h1 {
+          margin: 0;
+          padding: 0;
+
+          span {
+            &.special-letter {
+              color: $primary;
+              font-size: 2rem;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
