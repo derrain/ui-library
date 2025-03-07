b2f4a003ff0337aa1e331e31a4bff4e34abdc174
Derrain de Silva <derrain007@gmail.com>
2025-03-08 00:38:20 +1300

    Implemented Sidebar component and Index page

--- playground/app.vue
+++ playground/app.vue
@@ -5,6 +5,8 @@
 </template>
 
 <style lang="scss">
+  @use '~/assets/colors' as *;
+
   html,
   body {
     font-family: 'Manjari', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
@@ -13,5 +15,31 @@
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  body {
+    section {
+      padding-bottom: 1rem;
+    }
+
+    .page-header {
+      display: inline-block;
+      position: relative;
+      padding-bottom: 5px;
+
+      &::before {
+        content: "";
+        position: absolute;
+        width: 50%;
+        bottom: 0;
+        border-bottom: 2px solid $primary;
+      }
+    }
+
+    p {
+      font-size: 1.2rem;
+      font-weight: 400;
+      margin-top: 0;
+    }
   }
 </style>
--- playground/layouts/default.vue
+++ playground/layouts/default.vue
@@ -1,6 +1,32 @@
 <template>
-  <Navbar />
-  <main>
-    <slot />
-  </main>
+  <div class="main-wrapper">
+    <Navbar />
+    <main>
+      <Sidebar />
+      <div class="container">
+        <slot />
+      </div>
+    </main>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  @use '~/assets/colors' as *;
+
+  .main-wrapper {
+    height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr;
+
+    main {
+      display: grid;
+      grid-template-columns: 250px 1fr;
+
+      .container {
+        background: $darkgrey-2;
+        color: $white;
+        padding: 1.5rem 2rem;
+      }
+    }
+  }
+</style>
