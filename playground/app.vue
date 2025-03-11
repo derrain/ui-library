f82518ba33e7d92b566bd2dfb8dbf790745f284a
Derrain de Silva <derrain007@gmail.com>
2025-03-11 16:53:40 +1300

    Changed background colors of Playground sections, made properties flow inline and wrap responsively, added dynamic title and description change for pages

--- playground/app.vue
+++ playground/app.vue
@@ -1,3 +1,28 @@
+<script setup>
+  import { useRoute } from 'vue-router';
+  import { watchEffect } from 'vue';
+
+  const route = useRoute();
+  
+  watchEffect(() => {
+    const pathSegments = route.path.split('/').filter(Boolean);
+    const componentName = pathSegments.length > 0 ?
+      pathSegments[pathSegments.length - 1].replace(/-/g, ' ').replace(/\b\w/g, c => c.toUpperCase()) :
+      'Home';
+
+      useSeoMeta({
+        title: `NUED | ${componentName}`,
+        ogTitle: `NUED | ${componentName}`,
+        description: componentName === 'Home' ?
+          'NUED is a lightweight and customizable UI component library for Nuxt.js 3. Discover beautifully designed, reusable components built for seamless integration. Explore live demos, customize properties in real-time, and enhance your Nuxt projects with modern, accessible UI elements.' :
+          `Explore the ${componentName} component in NUED, a modular UI component library for Nuxt.js. View its live demo, customize properties, and access detailed implementation guides.`,
+        ogDescription: componentName === 'Home' ?
+          'NUED is a lightweight and customizable UI component library for Nuxt.js 3. Discover beautifully designed, reusable components built for seamless integration. Explore live demos, customize properties in real-time, and enhance your Nuxt projects with modern, accessible UI elements.' :
+          `Explore the ${componentName} component in NUED, a modular UI component library for Nuxt.js. View its live demo, customize properties, and access detailed implementation guides.`,
+      });
+  });
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
